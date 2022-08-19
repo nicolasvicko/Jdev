296554fdccc115f94c383f79231fd02e6efd9f1f
Nicolas Vicko <nicolasvicko@gmail.com>
2022-08-19 20:29:43 -0300

    9.10 - Os metódos comuns Getters e Setters

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -8,11 +8,20 @@ public class PrimeiraClasseJava {
 		
 		/* Objeto ainda não existe na memória, está invalido*/
 		Aluno aluno1 = new Aluno();
-		aluno1.nome = "João";
-		aluno1.idade = 50;
+		aluno1.setNome("João");
+		aluno1.setIdade(50);
+		aluno1.setDataNascimento("25/02/1993");
+		aluno1.setRegistroGeral("99.999.999.-6");
+		aluno1.setNumeroCpf("999.999.999-99");
+		aluno1.setNomeMae("Lene");
+		aluno1.setNomePai("cimar");
+		aluno1.setDataMatricula("19/08/2022");
+		aluno1.setNomeEscola("Oscar Brito");
+		aluno1.setSerieMatriculado("3");
 		
-		System.out.println("Nome do aluno1 é = " + aluno1.nome);
-		System.out.println("Idade do aluno1 é = " + aluno1.idade);
+		
+		System.out.println("Nome do aluno1 é = " + aluno1.getNome());
+		System.out.println("Idade do aluno1 é = " + aluno1.getIdade());
 		
 		
 		/* Agora temos um objeto real, na memória*/
