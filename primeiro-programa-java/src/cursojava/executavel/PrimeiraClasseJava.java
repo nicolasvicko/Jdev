aa576173415f587dc1d57bb9fe021ad99b923dbd
Nicolas Vicko <nicolasvicko@gmail.com>
2022-08-19 20:48:47 -0300

    9.11 - Nosso primeiro método de calculo de média de nota

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -18,10 +18,16 @@ public class PrimeiraClasseJava {
 		aluno1.setDataMatricula("19/08/2022");
 		aluno1.setNomeEscola("Oscar Brito");
 		aluno1.setSerieMatriculado("3");
+		aluno1.setNota1(90.4);
+		aluno1.setNota2(80.8);
+		aluno1.setNota3(70.9);
+		aluno1.setNota4(60.10);
+		
 		
 		
 		System.out.println("Nome do aluno1 é = " + aluno1.getNome());
 		System.out.println("Idade do aluno1 é = " + aluno1.getIdade());
+		System.out.println("Média da nota é = " + aluno1.getMediaNota());
 		
 		
 		/* Agora temos um objeto real, na memória*/
