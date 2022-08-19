35bf61d5a798671ccc72e37a1a826d15bec390c9
Nicolas Vicko <nicolasvicko@gmail.com>
2022-08-19 17:29:38 -0300

    Sobre o construtor de objeto

--- primeiro-programa-java/src/cursojava/classes/Aluno.java
+++ primeiro-programa-java/src/cursojava/classes/Aluno.java
@@ -1,5 +1,17 @@
 package cursojava.classes;
 
 public class Aluno {
+	
+	String nome;
+	int idade;
+	String dataNascimento;
+	String registroGeral;
+	String numeroCpf;
+	String nomeMae;
+	String nomePai;
+	String dataMatricula;
+	String nomeEscola;
+	String serieMatriculado;
+	
 
 }
