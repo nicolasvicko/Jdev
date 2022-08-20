01ef93aa93b24222b1850385ab3f4d5e70f70b2b
Nicolas Vicko <nicolasvicko@gmail.com>
2022-08-20 03:17:01 -0300

    9.21 - Adicionando disciplinas e calculando a médias das notas

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -46,14 +46,7 @@ public class PrimeiraClasseJava {
 		aluno1.setNomeEscola(NomeEscola);
 		aluno1.setSerieMatriculado(serieMatricula);
 		
-		aluno1.getDisciplina().setDisciplina1(disciplina1);
-		aluno1.getDisciplina().setNota1(Double.parseDouble(nota1));
-		aluno1.getDisciplina().setDisciplina2(disciplina2);
-		aluno1.getDisciplina().setNota2(Double.parseDouble(nota2));
-		aluno1.getDisciplina().setDisciplina3(disciplina3);
-		aluno1.getDisciplina().setNota3(Double.parseDouble(nota3));
-		aluno1.getDisciplina().setDisciplina4(disciplina4);
-		aluno1.getDisciplina().setNota4(Double.parseDouble(nota4));
+		
 		
 		
 		
